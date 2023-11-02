94a8d27a1d88ca36d7a73bd1e3ea25e42b4bfe9a
ayacat0809 <r202301890ov@jindai.jp>
2023-11-02 11:32:04 +0900

    20231102

--- de12/python/a.py
+++ de12/python/a.py
@@ -1,11 +1,27 @@
 from bs4 import BeautifulSoup
 
-with open('index.html' , mode=nt , encoding='uft-8') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
+url = r"file:///C:/Users/takep/mygit/xbp/de12/index.html"
 
-nt = soup.new_tag("li" , href='index.html')
-nt.string = 'aiueo'
+with open(url , mode="rt" , encoding="utf-8") as f:
+    soup = BeautifulSoup(f.read() , "html.parser")
 
-soup.find('ul').append(nt)
+# どんな内容のタグを追加するのか
+nt = soup.new_tag('a', href=r'https://amzn.asia/d/1hdxk8S')
+nt.string = 'Python自動処理の教科書'
 
-print(soup)
+# 追加
+soup.find(url).append(soup.new_tag)
+
+# with open( "index.html" , encoding='uft-8') as fp:
+#     html_str = fp.read()
+
+#     soup = BeautifulSoup(html_str , 'html.5lib')
+
+
+
+# nt = soup.new_tag("li" , href='index.html')
+# nt.string = 'aiueo'
+
+# soup.find('ul').append(nt)
+
+# print(soup)
